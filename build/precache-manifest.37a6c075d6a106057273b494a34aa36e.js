9f5dd08688f294d54078056dbf468d8d6ce26cee
shivachit1 <shivat@metropolia.fi>
2020-12-31 22:08:02 +0200

    uibuild

---
+++ build/precache-manifest.37a6c075d6a106057273b494a34aa36e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9926a9090a5589619656743b1adfcb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17c32c69f34c4f607bbe",
+    "url": "/static/css/main.db2ac9d3.chunk.css"
+  },
+  {
+    "revision": "b68e457976bcdcc45f75",
+    "url": "/static/js/2.ad7f1c86.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ad7f1c86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c32c69f34c4f607bbe",
+    "url": "/static/js/main.b3843272.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
